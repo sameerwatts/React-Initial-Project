bd3048bf3682a2157ab5b21e004dfd971d3e43d0
Sameer Watts <sameer.watts00@gmail.com>
2021-05-08 15:55:10 +0530

    Add comments

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,8 +1,8 @@
-import "./ExpenseItem.css";
+import "./ExpenseItem.css"; //import with css extension 
 
 const ExpenseItem = () => {
     return (
-        <div className="expense-item">
+        <div className="expense-item"> 
             <div>March 28th 2021</div>
             <div className="expense-item__description">
                 <h2>Car Insurance</h2>
@@ -13,3 +13,6 @@ const ExpenseItem = () => {
 };
 
 export default ExpenseItem;
+
+// 1. use root div
+// 2. use className instead of only class
