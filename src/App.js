6badf6d8807f054eff518a15ded9945458f29d50
Sameer Watts <sameer.watts00@gmail.com>
2021-05-08 23:28:20 +0530

    Update App.js without jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Expenses from "./components/Expenses/Expenses";
+import React from 'react';
 
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -23,12 +24,19 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}/>
-    </div>
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses }),
   );
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items={expenses}/>
+  //   </div>
+  // );
 }
 
 export default App;
