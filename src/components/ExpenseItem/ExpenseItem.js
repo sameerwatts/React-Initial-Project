b537200d36a6fd10eea5afa49874c48653c1d805
Sameer Watts <sameer.watts00@gmail.com>
2021-05-08 18:22:45 +0530

    Output dynamic data & working with jsx

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css"; //import with css extension 
 
 const ExpenseItem = () => {
+
+    const expenseTitle = "Car Insurance";
+    const expenseDate = new Date(2021, 4, 1);
+    const expenseAmount = 5000;
+
     return (
-        <div className="expense-item"> 
-            <div>March 28th 2021</div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">5000Rs</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}Rs</div>
             </div>
         </div>
     );
@@ -16,3 +21,6 @@ export default ExpenseItem;
 
 // 1. use root div
 // 2. use className instead of only class
+// 3. We don't have form for user input so we are adding constants in react function
+// 4. {} for adding basic js expression in jsx
+// 5. 
