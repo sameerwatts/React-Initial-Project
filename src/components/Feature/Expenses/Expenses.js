d8223d3ea465b8ff6c0a78ab2a0a800379ae95bd
Sameer Watts <sameer.watts00@gmail.com>
2021-05-10 19:40:56 +0530

    filter the expenses list

--- src/components/Feature/Expenses/Expenses.js
+++ src/components/Feature/Expenses/Expenses.js
@@ -7,21 +7,31 @@ import "./Expenses.css"
 
 const Expenses = (props) => {
 
-  const [filteredYear, setFilteredYear] = useState('');
+  const [filteredYear, setFilteredYear] = useState('2020');
 
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   }
+
+  const filteredExpenses = props.items.filter(item => {
+    const year = item.date.getFullYear().toString();
+    return year === filteredYear
+  })
+
+
+  const expenseItem = filteredExpenses.map(
+    expense => <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />);
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map(expense => <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />)};
+      {expenseItem}
     </Card>
   );
 };
