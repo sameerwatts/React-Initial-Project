9d3b86582d87a357a98ad7363dbca9b14041c605
Sameer Watts <sameer.watts00@gmail.com>
2021-05-16 22:20:45 +0530

    fix a small bug

--- src/components/Feature/NewExpense/ExpenseForm/ExpenseForm.js
+++ src/components/Feature/NewExpense/ExpenseForm/ExpenseForm.js
@@ -25,7 +25,7 @@ const ExpenseForm = (props) => {
         event.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate),
         }
         if (expenseData.title && expenseData.amount && !isNaN(expenseData.date.getTime())) {
