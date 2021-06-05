3af330b34da5bc2494e785361d1576b182ad8dfa
Sameer Watts <sameerwatts00@gmail.com>
2021-06-05 16:55:34 +0530

    Add card wrapper component

---
+++ Projects/add-user-practice/src/components/User/UserForm/UserForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../../UI/Card/Card";
+import classes from "./UserForm.module.css";
+
+const UserForm = () => {
+  return (
+    <Card>
+      <h1>Card component</h1>
+    </Card>
+  );
+};
+
+export default UserForm;
